bdbdcaddaa26b34786718debd45aa7b7d8d068ca
iceonomics <lonewolf980gaming@gmail.com>
2023-10-04 12:40:14 -0400

    Speedometer optimizations

---
+++ client.lua
@@ -0,0 +1,23 @@
+function DrawTextOnScreen(text)
+    SetTextProportional(0)
+    SetTextFont(7)
+    SetTextOutline()
+    SetTextScale(1.0,1.0)
+    SetTextColour(2,89,252, 255)
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(0.8, 0.95)
+end
+
+CreateThread(function()
+    while true do
+        local Speed = GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false)) * 3.6
+
+        if GetVehiclePedIsIn(GetPlayerPed(-1)) ~= 0 then
+            DrawTextOnScreen(string.format("%.1f", tostring(Speed)) .. " KM/H")
+        else
+            Wait(100)
+        end
+        Wait(0)
+    end
+end)
